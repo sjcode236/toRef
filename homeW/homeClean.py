6a5ab2f2804fef4e498631d2672b0af5230d36cd
sjcode236 <31339109+sjcode236@users.noreply.github.com>
2018-06-29 17:31:51 -0700

    Update homeClean.py

--- homeW/homeClean.py
+++ homeW/homeClean.py
@@ -2,7 +2,8 @@
 
 
 
-carpet & tilet clearning  9.per room, tile .25/sqft  6026090522 
+carpet & tilet clearning  9.per room, tile .25/sqft  
+6026090522 
 
 Carpet& tile  cleaning  , will give package price for 500  sqft
 4803716313
