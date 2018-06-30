fb1fb895506da441cc7b18f76acb55e38410d8e9
sjcode236 <31339109+sjcode236@users.noreply.github.com>
2018-06-29 18:10:01 -0700

    Update homeClean.py

--- homeW/homeClean.py
+++ homeW/homeClean.py
@@ -8,3 +8,16 @@ carpet & tilet clearning  9.per room, tile .25/sqft
 Carpet& tile  cleaning Jason , will give package price for 500  sqft , degrease and pressure cleaning
 4803716313
 
+home cleaning  
+
+Irma  and Abel , avondale ,  480 677 5251
+
+Kennedi Inez Slade
+https://www.facebook.com/kennedi.slade
+602-290-9011
+
+chemtech floor cleaning (tile & carpet )
+623 312 9405 
+5rooms  $100  for carepet  and Tile $25-50  per area
+
+
