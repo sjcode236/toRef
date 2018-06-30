6a0524ca57b21b602f21f333980dc4f053c3ff59
sjcode236 <31339109+sjcode236@users.noreply.github.com>
2018-06-30 13:50:16 -0700

    Update homeClean.py

--- homeW/homeClean.py
+++ homeW/homeClean.py
@@ -75,6 +75,11 @@ www.facebook.com/moxa.rivera.50/   , in phoenix
 Yard clean ups ; glendale 
 https://www.facebook.com/glen.cuevas.98
   
+Tony's VERY AFFORDABLE LANDSCAPER
+602 403 8681
+https://www.facebook.com/profile.php?id=100008285439460
+  https://www.facebook.com/crystal.raygoza.16
+phoenix 
   
   Clean Fill dirt plus delivery  $130
   Mike's excavating   ; https://www.facebook.com/michael.ciummo
