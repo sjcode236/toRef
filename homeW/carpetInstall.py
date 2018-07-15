c73df21c6218d31a48c1d1c6305fbab646264248
sjcode236 <31339109+sjcode236@users.noreply.github.com>
2018-07-15 16:56:15 -0700

    Update carpetInstall.py

--- homeW/carpetInstall.py
+++ homeW/carpetInstall.py
@@ -4,8 +4,19 @@
 
 
 
-Alfombras El Leon
-(602) 690-3786
-https://www.facebook.com/luisa.gomez.319
+Alfombras El Leon         
+(602) 690-3786            
+https://www.facebook.com/luisa.gomez.319             
+     
+       
+ New CARPET - high quality carpet one room at a time - various colors - various sizes $100 - $200              
+  https://www.facebook.com/dianagain          
+  https://www.facebook.com/marketplace/item/207465896640108/         
+    
+CARPET TILE SALES AND INSTALLATION       
+https://www.facebook.com/marketplace/item/397530434073041/          
+https://www.facebook.com/profile.php?id=100014223184362     
+Carmen Diaz  ;  Diaz Flooring   
+   602 722 8211  
 
-
+ 
