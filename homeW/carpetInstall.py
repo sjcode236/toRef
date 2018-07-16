b403484dbcee8837b6a118a0e16863ea419e24e2
sjcode236 <31339109+sjcode236@users.noreply.github.com>
2018-07-15 17:27:52 -0700

    Update carpetInstall.py

--- homeW/carpetInstall.py
+++ homeW/carpetInstall.py
@@ -16,7 +16,23 @@ https://www.facebook.com/luisa.gomez.319
 CARPET TILE SALES AND INSTALLATION       
 https://www.facebook.com/marketplace/item/397530434073041/          
 https://www.facebook.com/profile.php?id=100014223184362     
-Carmen Diaz  ;  Diaz Flooring   ; 19ave and baseline  store ; will come and give estimate 
-   602 722 8211  
+Carmen Diaz  ;  Diaz Flooring   ; 19ave and baseline  store ; will come and give estimate   
+   602 722 8211    
 
- 
+     
+Carpet installation** Commercial, Residential and sale
+https://www.facebook.com/marketplace/item/371483303339924/    
+https://www.facebook.com/marydominguez018    
+     Fabian 602.621.7250   
+     
+     
+     
+     
+     
+     
+
+HARDWOOD, LAMINATE, LUXURY VINYL PLANK & INSTALLATION ; Scottsdale, AZ     
+  https://www.facebook.com/FCIofScottsdale/    https://www.facebook.com/Galaxyremodeling    
+  https://www.facebook.com/sammysehak     
+         
+         
