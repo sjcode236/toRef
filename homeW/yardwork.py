9f3d57850b74d5b6725995e68a98c8897472a8de
sjcode236 <31339109+sjcode236@users.noreply.github.com>
2018-07-20 15:12:02 -0700

    Create yardwork.py

---
+++ homeW/yardwork.py
@@ -0,0 +1,27 @@
+
+
+=====================================================
+  
+  
+Yard clean ups ; glendale 
+https://www.facebook.com/glen.cuevas.98
+  
+Tony's VERY AFFORDABLE LANDSCAPER
+602 403 8681
+https://www.facebook.com/profile.php?id=100008285439460
+  https://www.facebook.com/crystal.raygoza.16
+phoenix 
+  
+  Clean Fill dirt plus delivery  $130
+  Mike's excavating   ; https://www.facebook.com/michael.ciummo
+
+
+Julio Gaxiola   623 313 4679    https://www.facebook.com/julio.gaxiola.1466
+backyard cleaning 
+backyard pavers, concrete & tile  
+Kitchen & bathrooms ,  remodeling works , 
+Roof  work 
+live in laveen 
+  
+ AJ / Desert Crown Landscape  cell: (602)-503-7474 
+  $45.00 Clean Up's
