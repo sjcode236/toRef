fecb73314a1788cca09688b755dd687cd1db6f0e
sjcode236 <31339109+sjcode236@users.noreply.github.com>
2018-08-08 23:23:20 -0700

    Update haircut.py

--- shop/haircut.py
+++ shop/haircut.py
@@ -8,7 +8,7 @@ haircut on foodcity plaza ,
 near 59 ave and bethenyhome road .
 
 
-LadyJanes Haircut 
+LadyJanes Haircut (Kennia )
 5800 w peoria ave , suite 103
 glendale , az , 85302 
 peoria and 59 ave ;  10 to 8pm , $15
