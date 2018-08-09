513d3ef2448a64662d1258f52a01b208304d6042
sjcode236 <31339109+sjcode236@users.noreply.github.com>
2018-08-08 23:21:29 -0700

    Create haircut.py

---
+++ shop/haircut.py
@@ -0,0 +1,15 @@
+
+
+
+
+
+
+haircut on foodcity plaza , 
+near 59 ave and bethenyhome road .
+
+
+LadyJanes Haircut 
+5800 w peoria ave , suite 103
+glendale , az , 85302 
+peoria and 59 ave ;  10 to 8pm , $15
+623 693 0298
