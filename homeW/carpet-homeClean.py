39b1d3c1f5c87f12254d7192afc96fca9d361add
sjcode236 <31339109+sjcode236@users.noreply.github.com>
2018-09-01 16:46:04 -0700

    Update carpet-homeClean.py

--- homeW/carpet-homeClean.py
+++ homeW/carpet-homeClean.py
@@ -82,47 +82,7 @@ $25/room"		http://www.keycarpetcleaningtechnology.com/
                                  
                                  
                                  
-=====home cleaning==============================================================================
 
-pumice  stone  to clean toilet 
-
-
-home cleaning  
-Irma  and Abel , avondale ,  480 677 5251
-
- 
-Sister's cleaning service , Tolleson, AZ
-https://www.facebook.com/martha.ydavid
-  602 668 6362  rafaela  (amiga)
-asked 90 for 9668 peoria home
-
-rosa Evelia  https://www.facebook.com/Hadassa777
-480 498 1863
-asked 80 for 9668 82nd ln ,
-two people  , amiga , live near I-10  
-  
-Exceptional House Cleaning
-Adina Hampton   https://www.facebook.com/adinainaz81
-glendale az  ,  
-$85  
-
-Move Out Cleaning  $139
-Candice Murray   480 331 5234  , 35 ave & olive ave
-https://www.facebook.com/candice.murray.3133
-pics show  getting very clean 
-80 for 9668 n 82nd lane  
-not good  work 
-
-Spotless House Cleaning
-Marjorie Barrera  480 819 6503 
-https://www.facebook.com/marjorie.ann.3192
-asked $50 for 9668 peroia   
-43 st & thomas 
-
-
-Orianna Moxaa Lopez  (only spanish )
-www.facebook.com/moxa.rivera.50/   , in phoenix 
-602 397 4779
 
 
 
